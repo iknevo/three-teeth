8414bcb6efc2b70cf1272e547769872e099bda8b
iknevo <nevoexists@gmail.com>
2026-03-05 20:38:39 +0200

    new model && interaction
    - new model where each teeth is a single mesh
    - hover and selected state with it's UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
 import { Canvas } from "@react-three/fiber";
 import { Center } from "@react-three/drei";
 import { OrbitControls } from "@react-three/drei";
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
 import { Model } from "./Scene";
+import { ModelNew } from "./new-scene";
 
 export default function App() {
+  const [selectedTooth, setSelectedTooth] = useState(null);
+  console.log({ selectedTooth });
   return (
     <>
       <Canvas
         camera={{ position: [0, 1, 5] }}
-        style={{ width: "100vw", height: "100vh" }}
+        style={{ width: "100vw", height: "95vh" }}
       >
         <ambientLight intensity={0.5} />
-        {/* <directionalLight position={[5, 10, 5]} /> */}
         <directionalLight position={[5, 10, 5]} intensity={1} />
         <directionalLight position={[-5, 5, -5]} intensity={0.6} />
         <hemisphereLight
@@ -23,12 +25,30 @@ export default function App() {
 
         <Suspense fallback={null}>
           <Center>
-            <Model />
+            {/* <Model /> */}
+            <ModelNew
+              selectedTooth={selectedTooth}
+              setSelectedTooth={setSelectedTooth}
+            />
           </Center>
         </Suspense>
 
         <OrbitControls />
       </Canvas>
+      <p
+        style={{
+          position: "absolute",
+          top: 20,
+          left: 20,
+          background: "white",
+          padding: "10px",
+          borderRadius: "6px"
+        }}
+      >
+        {selectedTooth?.toothName
+          ? `${selectedTooth.toothId} : ${selectedTooth.toothName}`
+          : "Click a tooth"}
+      </p>
     </>
   );
 }
